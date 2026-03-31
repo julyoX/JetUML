671ee5e99bd2ad35820ed48f344f475256287e60
Adriano Baumg <154255833+adrianobaumg@users.noreply.github.com>
2026-03-30 22:16:09 -0300

    Refactor DeserializationErrorAlert to use enum for categories

--- src/org/jetuml/gui/DeserializationErrorAlert.java
+++ src/org/jetuml/gui/DeserializationErrorAlert.java
@@ -21,54 +21,56 @@
 package org.jetuml.gui;
 
 import static org.jetuml.application.ApplicationResources.RESOURCES;
-
 import java.io.IOException;
-
 import org.jetuml.persistence.DeserializationException;
-
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 
-/**
- * Alert dialog specialized for displaying the various errors 
- * that can happen when opening a file.
- */
-public class DeserializationErrorAlert extends Alert
-{	
-	private static final String ROOT_RESOURCE_KEY_SUFFIX = "error.open_file_";
-	private static final String ROOT_RESOURCE_KEY_SUFFIX_IO = "io";
-	private static final String DETAILS_SUFFIX = "_details";
-	
-	/**
-	 * @param pException The exception received when opening a file.
-	 */
-	public DeserializationErrorAlert(Exception pException)
-	{
-		super(AlertType.ERROR, "", ButtonType.OK);
-		assert pException instanceof IOException || pException instanceof DeserializationException;
-		String keySuffix = determineResourceKeyPrefix(pException);
-		setTitle(RESOURCES.getString("alert.error.title"));
-		setHeaderText(RESOURCES.getString(ROOT_RESOURCE_KEY_SUFFIX + keySuffix));
-		
-		StringBuilder contentText = new StringBuilder(RESOURCES.getString(ROOT_RESOURCE_KEY_SUFFIX + keySuffix + DETAILS_SUFFIX));
-		contentText.append(" ").append(RESOURCES.getString("alert.error.details")).append(" ").append(pException.getMessage());
-		setContentText(contentText.toString());
-	}
-	
-	/*
-	 * Returns the key prefix in application resources for the header text and
-	 * details of the error. The suffix for the header text is the empty string,
-	 * and the suffix for the detail is "details"
-	 */
-	private static String determineResourceKeyPrefix(Exception pException)
-	{
-		if( pException instanceof IOException )
-		{
-			return ROOT_RESOURCE_KEY_SUFFIX_IO;
-		}
-		else
-		{
-			return ((DeserializationException)pException).category().toString().toLowerCase();
-		}
-	}
+public class DeserializationErrorAlert extends Alert {
+
+    private enum ErrorCategory {
+        IO("error.open_file_io"),
+        DESERIALIZATION("error.open_file_deserialization");
+
+        private final String baseKey;
+
+        ErrorCategory(String baseKey) {
+            this.baseKey = baseKey;
+        }
+
+        public String getHeaderKey() {
+            return baseKey;
+        }
+
+        public String getDetailsKey() {
+            return baseKey + "_details";
+        }
+    }
+
+    public DeserializationErrorAlert(Exception pException) {
+        super(AlertType.ERROR, "", ButtonType.OK);
+        assert pException instanceof IOException || pException instanceof DeserializationException;
+
+        ErrorCategory category = determineCategory(pException);
+
+        setTitle(RESOURCES.getString("alert.error.title"));
+        setHeaderText(RESOURCES.getString(category.getHeaderKey()));
+
+        StringBuilder contentText = new StringBuilder(RESOURCES.getString(category.getDetailsKey()));
+        contentText.append(" ")
+                   .append(RESOURCES.getString("alert.error.details"))
+                   .append(" ")
+                   .append(pException.getMessage());
+
+        setContentText(contentText.toString());
+    }
+
+
+    private static ErrorCategory determineCategory(Exception pException) {
+        if (pException instanceof IOException) {
+            return ErrorCategory.IO;
+        } else {
+            return ErrorCategory.DESERIALIZATION;
+        }
+    }
 }
